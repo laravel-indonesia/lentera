3745ba66effbe73f7bcb14edabf61e3d6687d85c
Edi Santoso <edicyber@gmail.com>
2015-02-22 20:27:17 +0700

    Create Abstract class for Repository

---
+++ app/Repositories/AbstractRepository.php
@@ -0,0 +1,38 @@
+<?php
+namespace App\Repositories;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+abstract class AbstractRepository
+{
+
+    /**
+     * The model to execute queries on.
+     *
+     * @var \Illuminate\Database\Eloquent\Model
+     */
+    protected $model;
+
+    /**
+     * Create a new repository instance.
+     *
+     * @param Model $model
+     */
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * Get a new instance of the model.
+     *
+     * @param  array $attributes
+     *
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function getNew(array $attributes = [])
+    {
+        return $this->model->newInstance($attributes);
+    }
+}
