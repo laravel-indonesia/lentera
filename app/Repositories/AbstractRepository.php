6fed831c081ca60b6e57f2cf0bd3b5bae2471457
Rifki Alhuraibi <riefky.alhuraibi@gmail.com>
2015-02-26 10:07:04 +0700

    fix indentation

--- app/Repositories/AbstractRepository.php
+++ app/Repositories/AbstractRepository.php
@@ -7,32 +7,33 @@ use Illuminate\Database\Eloquent\Model;
 abstract class AbstractRepository
 {
 
-    /**
-     * The model to execute queries on.
-     *
-     * @var \Illuminate\Database\Eloquent\Model
-     */
-    protected $model;
+		/**
+		 * The model to execute queries on.
+		 *
+		 * @var \Illuminate\Database\Eloquent\Model
+		 */
+		protected $model;
 
-    /**
-     * Create a new repository instance.
-     *
-     * @param Model $model
-     */
-    public function __construct(Model $model)
-    {
-        $this->model = $model;
-    }
+		/**
+		 * Create a new repository instance.
+		 *
+		 * @param Model $model
+		 */
+		public function __construct(Model $model)
+		{
+				$this->model = $model;
+		}
 
-    /**
-     * Get a new instance of the model.
-     *
-     * @param  array $attributes
-     *
-     * @return \Illuminate\Database\Eloquent\Model
-     */
-    public function getNew(array $attributes = [])
-    {
-        return $this->model->newInstance($attributes);
-    }
+		/**
+		 * Get a new instance of the model.
+		 *
+		 * @param  array $attributes
+		 *
+		 * @return \Illuminate\Database\Eloquent\Model
+		 */
+		public function getNew(array $attributes = [])
+		{
+				return $this->model->newInstance($attributes);
+		}
+
 }
